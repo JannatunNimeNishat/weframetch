7613a92f487f24da198da8742ef6a5acb325b32d
JannatunNimeNishat <nishatnime100@gmail.com>
2023-09-29 18:25:53 +0600

    sidebar added

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,4 @@
+"use client"
 import Image from "next/image";
 
 
@@ -21,14 +22,14 @@ export default function Home() {
             />
           </div>
           {/* navigation */}
-          <div className=" pl-10">
+          <div className=" px-10">
             <h3 className="font-bold text-[16px]">MAIN MENU</h3>
 
             <div className="mt-6">
 
 
               {/* dashboard */}
-              <div className="flex items-center gap-5">
+              <div className="flex items-center gap-5 py-7 cursor-pointer">
                 <Image
                   className="cursor-pointer"
                   src='/logo/dashboard.png'
@@ -39,10 +40,152 @@ export default function Home() {
                 <h3 className="text-[#464366] text-[18px] font-semibold">Dashboard</h3>
               </div>
 
+              {/* email */}
+              <div className='flex gap-5 w-full   items-center justify-between  cursor-pointer py-7'
+              >
+                <div className='flex gap-5'>
+                  <Image
+                    src={'/logo/email.png'}
+                    alt='email Icon'
+                    height={28}
+                    width={28}
+                  ></Image>
+                  <h3 className="text-[#464366] text-[18px] font-semibold">Email</h3>
+                </div>
+
+                <div className="flex gap-3">
+                  <div className="h-[29px] w-[29px] rounded-full bg-[#5ECFFF] flex items-center justify-center">
+                    <p className="">17</p>
+                  </div>
+                  <Image src={'/logo/down.png'} alt='down'
+                    height={28}
+                    width={28}
+                  ></Image>
+                </div>
+
+
+              </div>
+
+
+              {/* chat */}
+              <div className="flex items-center gap-5 py-7 cursor-pointer">
+                <Image
+                  className="cursor-pointer"
+                  src='/logo/chat.png'
+                  alt=""
+                  height={28}
+                  width={28}
+                />
+                <h3 className="text-[#7879F1] text-[18px] font-semibold">Chat</h3>
+              </div>
+
+              {/* Kanban */}
+              <div className='flex gap-5 w-full   items-center justify-between  cursor-pointer py-7'
+              >
+                <div className='flex gap-5'>
+                  <Image
+                    src={'/logo/kanban.png'}
+                    alt='kanban Icon'
+                    height={28}
+                    width={28}
+                  ></Image>
+                  <h3 className="text-[#6418C3] text-[18px] font-semibold">Kanban</h3>
+                </div>
+
+                <Image src={'/logo/up.png'} alt='down'
+                  height={28}
+                  width={28}
+                ></Image>
 
 
 
+              </div>
 
+              {/* contact */}
+              <div className='flex gap-5 w-full   items-center justify-between  cursor-pointer py-7'
+              >
+                <div className='flex gap-5'>
+                  <Image
+                    src={'/logo/contact.png'}
+                    alt='contact Icon'
+                    height={28}
+                    width={28}
+                  ></Image>
+                  <h3 className="text-[#7879F1] text-[18px] font-semibold">Contact</h3>
+                </div>
+
+                <div className="h-[35px] w-[63px] rounded-full bg-[#E328AF] flex items-center justify-center">
+                  <p className="">NEW</p>
+                </div>
+              </div>
+
+              {/* calendar */}
+              <div className="flex items-center gap-5 py-7 cursor-pointer">
+                <Image
+                  className="cursor-pointer"
+                  src='/logo/calendar.png'
+                  alt=""
+                  height={28}
+                  width={28}
+                />
+                <h3 className="text-[#7879F1] text-[18px] font-semibold">Calendar</h3>
+              </div>
+              {/* courses */}
+              <div className="flex items-center gap-5 py-7 cursor-pointer">
+                <Image
+                  className="cursor-pointer"
+                  src='/logo/courses.png'
+                  alt=""
+                  height={28}
+                  width={28}
+                />
+                <h3 className="text-[#7879F1] text-[18px] font-semibold">Courses</h3>
+              </div>
+
+              {/* shop */}
+              <div className="flex items-center gap-5 py-7 cursor-pointer">
+                <Image
+                  className="cursor-pointer"
+                  src='/logo/shop.png'
+                  alt=""
+                  height={28}
+                  width={28}
+                />
+                <h3 className="text-[#7879F1] text-[18px] font-semibold">Shop</h3>
+              </div>
+
+              {/* invoices */}
+              <div className="flex items-center gap-5 py-7 cursor-pointer">
+                <Image
+                  className="cursor-pointer"
+                  src='/logo/invoices.png'
+                  alt=""
+                  height={28}
+                  width={28}
+                />
+                <h3 className="text-[#7879F1] text-[18px] font-semibold">Invoices</h3>
+              </div>
+
+              {/* setting */}
+              <div className="flex items-center gap-5 py-7 cursor-pointer">
+                <Image
+                  className="cursor-pointer"
+                  src='/logo/setting.png'
+                  alt=""
+                  height={28}
+                  width={28}
+                />
+                <h3 className="text-[#7879F1] text-[18px] font-semibold">Setting</h3>
+              </div>
+
+              {/* increase your work with kanban */}
+              <Image
+              className="mt-[80px] mb-[100px] cursor-pointer" 
+                src={'/logo/increase_your_work_with_kanban.png'}
+                width={245}
+                height={203}
+                alt="banner image"
+              />
 
 
             </div>
